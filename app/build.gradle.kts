548001257c314b4d5d3886fbe7ffa9ec61b85c59
BlockSmith <53007606+LifeLane@users.noreply.github.com>
2026-08-24 19:51:33 +0530

    build: update application ID to com.mitsara.swayam

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.edgeaicore.vqkmtz"
+    applicationId = "com.mitsara.swayam"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
