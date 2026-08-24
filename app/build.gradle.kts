f84c8d1b9f9c9000edc21836ac53b69e9de68442
BlockSmith <53007606+LifeLane@users.noreply.github.com>
2026-08-25 02:10:15 +0530

    Update version code and version name in build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.mitsara.swayam"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode 2
+        versionName "2.1.0-beta02"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
